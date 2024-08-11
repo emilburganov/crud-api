a3ff8c23e60b942d4ca399043569ff5c18f7b2db
emilburganov <emil.burganov03@gmail.com>
2024-08-11 19:23:21 +0300

    feat: swagger installed

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,16 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *    title="CRUD API",
+ *    description="An API of for test task in FillinCode company.",
+ *    version="1.0.0",
+ * ),
+ * @OA\PathItem(
+ *     path="/api/"
+ * )
+ */
 abstract class Controller
 {
     //
