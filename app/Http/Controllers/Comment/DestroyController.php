0e3dd84d9a151fa70df48d9c830deea71080762a
emilburganov <emil.burganov03@gmail.com>
2024-08-11 23:08:12 +0300

    feat: swagger for comment model

--- app/Http/Controllers/Comment/DestroyController.php
+++ app/Http/Controllers/Comment/DestroyController.php
@@ -3,17 +3,26 @@
 namespace App\Http\Controllers\Comment;
 
 use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class DestroyController extends BaseController
 {
     /**
-     * @param Comment $comment
+     * @param int $id
      * @return JsonResponse
      */
-    public function __invoke(Comment $comment): JsonResponse
+    public function __invoke(int $id): JsonResponse
     {
+        $comment = Comment::find($id);
+
+        if (empty($comment)) {
+            return response()->json([
+                "message" => "Comment not found."
+            ], Response::HTTP_NOT_FOUND);
+        }
+
         $this->service->destroy($comment);
 
         return response()->json([], Response::HTTP_NO_CONTENT);
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 /**
  * @OA\Info(
  *    title="CRUD API",
- *    description="An API of for test task in FillinCode company.",
+ *    description="An API for test task in FillinCode company.",
  *    version="1.0.0",
  * ),
  * @OA\PathItem(
